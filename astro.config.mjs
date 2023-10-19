e28a64c449103db37c9adb327b0693767b8a1855
natoxpy <natoxpy@gmail.com>
2023-10-18 20:22:40 -0400

    Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 
@@ -6,5 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), vue(), react()]
+  integrations: [svelte(), vue(), react()],
+  site: "https://toxpy_s_org.github.io",
+  base: "/blog-template",
 });
